094a41acc6a72ac1d75baafd0a800d65f7215ec5
YanasuLucaroniW10 <81329768+YanasuLucaroni@users.noreply.github.com>
2023-01-24 20:33:02 -0300

    a

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from "./logo.svg";
 import "./App.css";
+import React from "react";
+
+const todos = [
+  { text: "Cortar cebolla", completed: false },
+  { text: "Tomar curso de react", completed: false },
+  { text: "Llorar por la llorona", completed: false },
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Aplicación react
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      {/* <TodoCounter /> */}
+      <h2>Has completado 2 de 3 TODOs</h2>
+      {/* <TodoSearch /> */}
+      <input placeholder="Cebolla" />
+
+      {/* <TodoList>
+        {todos.map((todo) => (
+          <TodoItem />
+        ))}
+      </TodoList> */}
+      {/* <CreateTodoButton /> */}
+      <button>+</button>
+    </React.Fragment>
   );
 }
 
